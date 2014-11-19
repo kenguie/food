c8c6648a4d0c4cfd104168a11035b5e8607ba756
nyhunter77 <nyhunter77@yahoo.com>
2014-11-19 16:09:46 -0500

    image works !

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,7 @@ class User < ActiveRecord::Base
     [reststreet, restcity, reststate, restzip].compact.join(', ')
   end
 
+  has_attached_file :avatar, :default_url => "missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 end
